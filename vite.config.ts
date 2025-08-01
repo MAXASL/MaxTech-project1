e52da57e017094c081cfc2d80ba8a84da4811bf8
Maxas <maxaslukwesa0@gmail.com>
2025-08-01 12:05:42 +0200

    prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
+  base: 'MaxTech-project1',
   plugins: [
     react(),
     mode === 'development' &&
